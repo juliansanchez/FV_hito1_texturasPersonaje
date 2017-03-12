2cc7ead70b96504e4c01d352211fb32951dba440
juliansanchez <eljovenjuli@hotmail.com>
2017-03-12 22:36:37 +0100

    rama con diagonal y texturas

--- texturaPersonaje/main.cpp
+++ texturaPersonaje/main.cpp
@@ -1,36 +1,63 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
+/*!
+ * \file    main.cpp
+ * \brief   move a sprite with the keyboard (http://www.lucidarme.me/?p=6127)
+ * \author  Philippe Lucidarme
+ * \version 1.0
+ * \date    12/18/2016
+ */
 
-#define kVel 5
+// SFML libraries
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <math.h>
+
+// Sprite speed (high values = high speed)
+#define SPRITE_SPEED  2
 
 int main()
 {
+    // _____________________
+    // ::: Create window :::
+
     int altoPantalla = 540;
     int anchoPantalla = 900;
     
     //Creamos una ventana 
     sf::RenderWindow window(sf::VideoMode(anchoPantalla, altoPantalla), "Hito 1: animacion personaje");
 
-    //Cargo la imagen donde reside la textura del sprite
-    sf::Texture tex;
-    if (!tex.loadFromFile("resources/isaacAzul.png"))
+    // Enable vertical sync. (vsync)
+    window.setVerticalSyncEnabled (true);
+    // When a key is pressed, sf::Event::KeyPressed will be true only once
+    window.setKeyRepeatEnabled(false);
+
+
+    // ____________________
+    // ::: Load texture :::
+
+    // Create texture from PNG file
+    sf::Texture texture;
+    if (!texture.loadFromFile("resources/isaacAzul.png"))
     {
-        std::cerr << "Error cargando la imagen sprites.png";
-        exit(0);
+        std::cerr << "Error while loading texture" << std::endl;
+        return -1;
     }
-    
-    //Y creo el spritesheet a partir de la imagen anterior
-    sf::Sprite cabeza(tex);
-    sf::Sprite piernas(tex);
+    // Enable the smooth filter. The texture appears smoother so that pixels are less noticeable.
+    // texture.setSmooth(true);
+
+    // ::: Create sprite and apply texture :::
+
+  
+        //Y creo el spritesheet a partir de la imagen anterior
+    sf::Sprite cabeza(texture);
+    sf::Sprite piernas(texture);
     int tamCabeza = 32;
     int radioCabeza = tamCabeza/2;
     int tamPiernas = 32;
-    int radioPiernas = (tamPiernas/2)+4; // modificar +4 si se cambia la escala
-    
-    int escalCab = 2;
-    int escalPie = 4.5;
-    //Le pongo el centroide donde corresponde
-    
+    int radioPiernas = (tamPiernas/2)-4; // modificar +4 si se cambia la escala
+    // para cambiar el tamaño de los sprites
+    int escalCab = 1;
+    int escalPie = 1.5;
+       //Le pongo el centroide donde corresponde
     cabeza.setOrigin(tamCabeza/2,tamCabeza/2);
     piernas.setOrigin(tamPiernas/2,tamPiernas/2);
     //Cojo el sprite que me interesa por defecto del sheet
@@ -39,104 +66,129 @@ int main()
     // Lo dispongo en el centro de la pantalla
     cabeza.setPosition(anchoPantalla/2, altoPantalla/2);
     piernas.setPosition(anchoPantalla/2, altoPantalla/2+radioPiernas);
-    
+    // tamaño de los esprites
     cabeza.setScale(escalCab,escalCab);
     piernas.setScale(escalPie,escalPie);
 
-    
 
-    //Bucle del juego
+    // Coordenadas para los sprites
+    int x=window.getSize().x/2.;
+    int y=window.getSize().y/2.;
+
+    // avisadores de tecla pulsada
+    bool upFlag=false;
+    bool downFlag=false;
+    bool leftFlag=false;
+    bool rightFlag=false;
+    
+    // ::: INICIO LOOP :::
     while (window.isOpen())
     {
-        //Bucle de obtención de eventos
+        // Process events
         sf::Event event;
         while (window.pollEvent(event))
         {
-            
-            switch(event.type){
-                
-                //Si se recibe el evento de cerrar la ventana la cierro
-                case sf::Event::Closed:
-                    window.close();
+            // Close the window if a key is pressed or if requested
+            if (event.type == sf::Event::Closed) 
+                window.close();
+
+            // If a key is pressed
+            if (event.type == sf::Event::KeyPressed)
+            {
+                switch (event.key.code)
+                {
+                // If escape is pressed, close the application
+                case  sf::Keyboard::Escape : window.close(); break;
+
+                // Process the up, down, left and right keys
+                case sf::Keyboard::W : // ARRIBA     
+                    upFlag=true; 
+                    cabeza.setTextureRect(sf::IntRect(5*tamCabeza, 0*tamCabeza, tamCabeza, tamCabeza));
+                    piernas.setTextureRect(sf::IntRect(1*tamPiernas,1*tamPiernas, tamPiernas, tamPiernas));
+                    piernas.setScale(-escalPie,escalPie);
                     break;
-                case sf::Event::KeyReleased:
-                            cabeza.setTextureRect(sf::IntRect(0*tamCabeza, 0*tamCabeza, tamCabeza, tamCabeza));
-                            piernas.setTextureRect(sf::IntRect(0*tamPiernas, 1*tamPiernas, tamPiernas, tamPiernas));
-                            //Escala por defecto
-                            cabeza.setScale(escalCab,escalCab);
-                          
-                break;
                     
-                //Se pulsó una tecla, imprimo su codigo
-                case sf::Event::KeyPressed:
+                case sf::Keyboard::S:// ABAJO
+                    downFlag=true;
+                    cabeza.setTextureRect(sf::IntRect(1*tamCabeza, 0*tamCabeza, tamCabeza, tamCabeza));     
+                    piernas.setTextureRect(sf::IntRect(1*tamPiernas,1*tamPiernas, tamPiernas, tamPiernas));
+                    piernas.setScale(escalPie,escalPie);
+                    break;
                     
-                    //Verifico si se pulsa alguna tecla de movimiento
-                    switch(event.key.code) {
-                        
-                        //Mapeo del cursor
-                        
-                        case sf::Keyboard::Right:
-                            cabeza.setTextureRect(sf::IntRect(2*tamCabeza, 0*tamCabeza, tamCabeza, tamCabeza));
-                            //Escala por defecto
-                            cabeza.setScale(escalCab,escalCab);
-                            cabeza.move(kVel,0);
-                            piernas.setTextureRect(sf::IntRect(3*tamPiernas,2*tamPiernas, tamPiernas, tamPiernas));
-                            piernas.move(kVel,0);
-                            piernas.setScale(escalPie,escalPie);
-                        break;
-
-                        case sf::Keyboard::Left:
-                            cabeza.setTextureRect(sf::IntRect(2*tamCabeza, 0*tamCabeza, tamCabeza, tamCabeza));
-                            //Reflejo vertical
-                            cabeza.setScale(-escalCab,escalCab);
-                            cabeza.move(-kVel,0); 
-                            piernas.setTextureRect(sf::IntRect(3*tamPiernas,2*tamPiernas, tamPiernas, tamPiernas));
-                            piernas.setScale(-escalPie,escalPie);
-                            piernas.move(-kVel,0);
+                case sf::Keyboard::A: // IZDA   
+                    leftFlag=true; 
+                    cabeza.setTextureRect(sf::IntRect(2*tamCabeza, 0*tamCabeza, tamCabeza, tamCabeza));
+                    //Reflejo vertical
+                    cabeza.setScale(-escalCab,escalCab);
+                    piernas.setTextureRect(sf::IntRect(3*tamPiernas,2*tamPiernas, tamPiernas, tamPiernas));
+                    piernas.setScale(-escalPie,escalPie);
+                    break;
                             
-                            
-                            
-                        break;
-                        
-                        case sf::Keyboard::Up:
-                            cabeza.setTextureRect(sf::IntRect(5*tamCabeza, 0*tamCabeza, tamCabeza, tamCabeza));
-                            cabeza.move(0,-kVel); 
-                            
-                            piernas.setTextureRect(sf::IntRect(1*tamPiernas,1*tamPiernas, tamPiernas, tamPiernas));
-                            piernas.move(0,-kVel);
-                            piernas.setScale(-escalPie,escalPie);
-                        break;
-                        
-                        case sf::Keyboard::Down:
-                            cabeza.setTextureRect(sf::IntRect(1*tamCabeza, 0*tamCabeza, tamCabeza, tamCabeza));
-                            cabeza.move(0,kVel); 
-                            piernas.setTextureRect(sf::IntRect(1*tamPiernas,1*tamPiernas, tamPiernas, tamPiernas));
-                            piernas.move(0,kVel);
-                            piernas.setScale(escalPie,escalPie);
-                        break;
-                        
-                 
-                        //Tecla ESC para salir
-                        case sf::Keyboard::Escape:
-                            window.close();
-                        break;
-                        
-                        //Cualquier tecla desconocida se imprime por pantalla su código
-                        default:
-                            std::cout << event.key.code << std::endl;
-                        break;
-                              
-                    }
-
+                case sf::Keyboard::D: // DERECHA
+                    rightFlag=true; 
+                    cabeza.setTextureRect(sf::IntRect(2*tamCabeza, 0*tamCabeza, tamCabeza, tamCabeza));
+                    //Escala por defecto
+                    cabeza.setScale(escalCab,escalCab);
+                    piernas.setTextureRect(sf::IntRect(3*tamPiernas,2*tamPiernas, tamPiernas, tamPiernas));
+                    piernas.setScale(escalPie,escalPie);
+                    break;
+                    
+                default : break;
+                }
             }
-            
+
+            // If a key is released
+            if (event.type == sf::Event::KeyReleased)
+            {
+                // posicion del sprite cuando no se pulsa el teclado
+                cabeza.setTextureRect(sf::IntRect(0*tamCabeza, 0*tamCabeza, tamCabeza, tamCabeza));
+                piernas.setTextureRect(sf::IntRect(0*tamPiernas, 1*tamPiernas, tamPiernas, tamPiernas));
+                //Escala por defecto
+                cabeza.setScale(escalCab,escalCab);
+                
+                switch (event.key.code)
+                {
+                // Process the up, down, left and right keys
+                case sf::Keyboard::W : // ARRIBA     
+                    upFlag=false; break;
+                case sf::Keyboard::S:  // ABAJO  
+                    downFlag=false; break;
+                case sf::Keyboard::A:  // IZDA  
+                    leftFlag=false; break;
+                case sf::Keyboard::D:  // DERECHA
+                    rightFlag=false; break;
+                default : break;
+                }
+            }
         }
-        window.clear();
+
+        // Update coordinates
+        if (leftFlag) x-=SPRITE_SPEED;
+        if (rightFlag) x+=SPRITE_SPEED;
+        if (upFlag) y-=SPRITE_SPEED;
+        if (downFlag) y+=SPRITE_SPEED;
+
+        // Controlar colisión con paredes
+        if (x<0) 
+            x=radioCabeza;
+        if (x>(int)window.getSize().x) 
+            x=window.getSize().x-radioCabeza;
+        if (y<0) 
+            y=radioCabeza;
+        if (y>(int)window.getSize().y) 
+            y=window.getSize().y-radioCabeza;
+
+        // Clear the window and apply grey background
+        window.clear( sf::Color(127,127,127));
+
+        // Rotate and draw the sprite
+        cabeza.setPosition(x,y);
+        piernas.setPosition(x,y+12); // valor para ajustar cuerpo a cabeza
         window.draw(piernas);
         window.draw(cabeza);
-        
+
+        // Update display and wait for vsync
         window.display();
     }
-
     return 0;
 }
