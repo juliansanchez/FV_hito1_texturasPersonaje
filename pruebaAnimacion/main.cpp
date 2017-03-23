dff303e19bb82f1324b222fbc48dea5cb0eb5197
juliansanchez <eljovenjuli@hotmail.com>
2017-03-23 23:24:11 +0100

    Animacion basica OK

---
+++ pruebaAnimacion/main.cpp
@@ -0,0 +1,119 @@
+/*!
+ * \file    main.cpp
+ * \brief   move a sprite with the keyboard (http://www.lucidarme.me/?p=6127)
+ * \author  Philippe Lucidarme
+ * Sprite del personaje y animacion: JULIAN SANCHEZ GARCIA 
+ */
+
+// SFML libraries
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+// Sprite speed (high values = high speed)
+#define SPRITE_SPEED  1
+
+int main()
+{
+
+    float velocidad = 5;
+    int contadorPasos = 2;
+    // ::: VENTANA PRINCIPAL :::
+    sf::RenderWindow window(sf::VideoMode(720, 540), "Hito 1: animacion personaje");
+ 
+    // ::: Creamos y cargamos las texturas :::
+    sf::Texture texture;
+    if (!texture.loadFromFile("resources/isaacAzul.png"))
+    {
+        std::cerr << "Error while loading texture" << std::endl;
+        return -1;
+    }
+    
+    sf::Sprite personaje(texture);
+    personaje.setPosition(window.getSize().x/2,window.getSize().y/2);
+    personaje.setTextureRect(sf::IntRect(0,0,32,32));
+   
+ 
+      
+    // ::: INICIO LOOP :::
+    while (window.isOpen())
+    {
+        // Proceso de eventos
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Cerrar ventana
+            if (event.type == sf::Event::Closed) 
+                window.close();
+
+            // Si pulsamos una tecla
+            if (event.type == sf::Event::KeyPressed)
+            {
+                switch (event.key.code)
+                {
+                // Para cerrar la ventana con ESC
+                case  sf::Keyboard::Escape : window.close(); break;
+
+                // Teclas  up, down, left and right keys
+                case sf::Keyboard::W : // ARRIBA     
+                    personaje.move(0,-velocidad);
+                    personaje.setTextureRect(sf::IntRect(32*4,0,32,32));
+                    
+                    break;
+                    
+                case sf::Keyboard::S:// ABAJO
+                    personaje.move(0,velocidad);
+                    personaje.setTextureRect(sf::IntRect(32*1,0,32,32));
+                    
+                    break;
+                    
+                case sf::Keyboard::A: // IZDA   
+                   personaje.move(-velocidad,0);
+                   personaje.setTextureRect(sf::IntRect(32*2,0,32,32));
+                   personaje.setScale(-1,1);
+                    
+                    break;
+                            
+                case sf::Keyboard::D: // DERECHA
+                    personaje.move(velocidad,0);
+                    personaje.setTextureRect(sf::IntRect(contadorPasos*32,0,32,32));
+                    contadorPasos++;
+                    if(contadorPasos == 4){
+                        contadorPasos = 2;
+                    }
+                    break;
+                    
+                default : break;
+                }
+                
+            }
+
+            // Si no pulsamos ninguan tecla
+            if (event.type == sf::Event::KeyReleased)
+            {
+                                
+                
+
+              
+            }
+        }
+
+
+
+        
+        
+        // Limpiamos la ventana y aplicamos un color de fondo 
+        window.clear( sf::Color(127,127,127));
+       
+        
+        
+        window.draw(personaje);
+ 
+
+        // Actualizar mostrar por pantalla
+        window.display();
+    }
+    return 0;
+}
---
+++ pruebaAnimacion/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=pruebaanimacion
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/pruebaanimacion
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=pruebaanimacion.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/pruebaanimacion.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=pruebaanimacion
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/pruebaanimacion
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=pruebaanimacion.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/pruebaanimacion.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
